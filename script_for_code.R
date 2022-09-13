f43f438f1ed5b5c55d2b313070d9907523ec29d8
Amarachi Akunna <akunna1@live.unc.edu>
2022-09-13 18:51:20 -0400

    Saving files- 13th Sept 2022

---
+++ script_for_code.R
@@ -0,0 +1,33 @@
+# Set working directory to Source File Location
+# The CSV files from my desktop
+
+# Load libraries
+library(tidyverse)  # dealing with tabular data
+library(lubridate) # handling dates
+library(dplyr) # data manipulation
+library(tidyr) # tidy data
+library(data.table) # table formated result
+library(ggplot2) # visualization
+library(scales)# graphical scaling
+
+# Importing my .csv data files to the Global Environment
+eui_data_2019 = read_csv("eui2019_ake.csv")
+br_data_2019 = read_csv("buildings_and_renos_2021_ake.csv")
+
+eui_data_joined = left_join(eui_data_2019, br_data_2019, by="Building_number")
+
+view(eui_data_joined)
+
+#Filtering to remove rows with NAs
+# Removing the rows of column year_construction_started containing NAs
+eui_data_n1 <- filter(eui_data_joined, !is.na(eui_data_joined$Year_construction_started))
+view(eui_data_n1) #gives 234 entries
+
+# Removing the rows of column Year_construction_started containing NAs
+eui_data_n2 <- filter(eui_data_n1, !is.na(eui_data_n1$Latest_reno))
+view(eui_data_n2) #gives 117 entries
+
+
+#Plotting
+ggplot()+
+  geom_point(data = eui_data_n2, mapping = aes(x=Year_construction_started, y=Latest_reno))
