bcb6e5bdc87b16cbdbddcc11c19417f67a242c60
Amarachi Akunna <akunna1@live.unc.edu>
2022-09-20 11:39:51 -0400

    Make some changes to graph- 20th Sept

--- script_for_code.R
+++ script_for_code.R
@@ -16,18 +16,22 @@ br_data_2019 = read_csv("buildings_and_renos_2021_ake.csv")
 
 eui_data_joined = left_join(eui_data_2019, br_data_2019, by="Building_number")
 
-view(eui_data_joined)
+view(eui_data_joined) # contains 252 entries
 
 #Filtering to remove rows with NAs
-# Removing the rows of column year_construction_started containing NAs
-eui_data_n1 <- filter(eui_data_joined, !is.na(eui_data_joined$Year_construction_started))
-view(eui_data_n1) #gives 234 entries
+# Removing the rows of Year_occupied containing NAs
+eui_data_n1 <- filter(eui_data_joined, !is.na(eui_data_joined$Year_occupied))
+view(eui_data_n1) #gives 236 entries
 
-# Removing the rows of column Year_construction_started containing NAs
+# Removing the rows of column Latest_reno containing NAs
 eui_data_n2 <- filter(eui_data_n1, !is.na(eui_data_n1$Latest_reno))
 view(eui_data_n2) #gives 117 entries
 
 
 #Plotting
 ggplot()+
-  geom_point(data = eui_data_n2, mapping = aes(x=Year_construction_started, y=Latest_reno))
+  geom_point(data = eui_data_n2, mapping = aes(x=Year_occupied, y=Latest_reno))
+  # make correlation line later
+
+
+#make box plot for steam, EUI and Chilled water. Do group_by property type
