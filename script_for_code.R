ed1564c1debe7c1f9f1d1aa3c64dfcf5fdfa4f41
Amarachi Akunna <akunna1@live.unc.edu>
2022-09-21 18:44:07 -0400

    Created SCE boxplots- 21st Sept

--- script_for_code.R
+++ script_for_code.R
@@ -6,7 +6,7 @@ library(tidyverse)  # dealing with tabular data
 library(lubridate) # handling dates
 library(dplyr) # data manipulation
 library(tidyr) # tidy data
-library(data.table) # table formated result
+library(data.table) # table formatted result
 library(ggplot2) # visualization
 library(scales)# graphical scaling
 
@@ -18,35 +18,52 @@ eui_data_joined = left_join(eui_data_2019, br_data_2019, by="Building_number")
 
 view(eui_data_joined) # contains 252 entries
 
-#Filtering to remove rows with NAs
-# Removing the rows of Year_occupied containing NAs
-eui_data_n1 <- filter(eui_data_joined, !is.na(eui_data_joined$Year_occupied))
-view(eui_data_n1) #gives 236 entries
+#removing NAs in Steam_in_klb, Chilled_water_ton_hr and EUI_in_mmbtu_per_sqft columns
+eui_data_steam <- filter(eui_data_joined, !is.na(eui_data_joined$Steam_in_klb))
+eui_data_sc <- filter(eui_data_steam, !is.na(eui_data_steam$Chilled_water_ton_hr))
+eui_data_sce <- filter(eui_data_sc, !is.na(eui_data_sc$EUI_in_mmbtu_per_sqft))
+view(eui_data_sce) #gives 126 entries
 
-# Removing the rows of column Latest_reno containing NAs
+#Grouping joined data (unfiltered) by property type and doing a count
+property_type_count <- eui_data_joined%>%
+  group_by(Property_type) %>%
+  summarise(Total = n()) # grouping the joined data into property type by count
+view(property_type_count)
+
+#Grouping sce data  by property type and doing a count
+#sce- steam,chilled water and eui
+property_type_count_sce <- eui_data_sce%>%
+  group_by(Property_type) %>%
+  summarise(Total = n()) # grouping the steam data into property type by count
+view(property_type_count_sce)
+
+#Boxplots
+#boxplot1: Steam
+qplot(x=eui_data_sce$Property_type, y=eui_data_sce$Steam_in_klb, geom="boxplot", main="Boxplot of Steam (klb) by Property type", ylab="Steam(klb)", xlab="Property Type", col=I('black'), fill=eui_data_sce$Property_type) + 
+  coord_flip()+
+  theme(legend.position="none") #to remove legend
+  
+#boxplot2: Chilled Water
+qplot(x=eui_data_sce$Property_type, y=eui_data_sce$Chilled_water_ton_hr, geom="boxplot", main="Boxplot of Chilled Water (ton-hr) by Property type", ylab="Chilled Water (ton-hr)", xlab="Property Type", col=I('black'), fill=eui_data_sce$Property_type) + 
+  coord_flip()+
+  theme(legend.position="none") #to remove legend
+
+#boxplot3: EUI
+qplot(x=eui_data_sce$Property_type, y=eui_data_sce$EUI_in_mmbtu_per_sqft, geom="boxplot", main="Boxplot of EUI (MMBTU/Sqft) by Property type", ylab="EUI (MMBTU/Sqft)", xlab="Property Type", col=I('black'), fill=eui_data_sce$Property_type) + 
+  coord_flip()+
+  theme(legend.position="none") #to remove legend
+
+#_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+#Line Graph for Year Occupied Vs. Year of Latest Renovation
+#Filtering to remove rows with NAs for columns Year_occupied and Latest_reno
+eui_data_n1 <- filter(eui_data_sce, !is.na(eui_data_sce$Year_occupied))
+view(eui_data_n1) #gives 123 entries
+
 eui_data_n2 <- filter(eui_data_n1, !is.na(eui_data_n1$Latest_reno))
-view(eui_data_n2) #gives 117 entries
-
+view(eui_data_n2) #gives 74 entries
 
 #Plotting
 ggplot()+
   geom_point(data = eui_data_n2, se=FALSE, color='black', mapping = aes(x=Year_occupied, y=Latest_reno))+
-  geom_smooth(method = 'lm', data = eui_data_n2, mapping = aes(x=Year_occupied, y=Latest_reno))+
   labs(x='Year Occupied', y='Year of Latest Renovation', title='Latest Renovation Year Vs. Occupancy Year')
-
-
-#Make box plot for steam, EUI and Chilled water. Do group_by property type
-
-#Box Plot for steam
-#removing NAs in Steam_in_klb column
-eui_data_steam <- filter(eui_data_joined, !is.na(eui_data_joined$Steam_in_klb))
-view(eui_data_steam) #gives 147 entries
-
-#Grouping joined data (unfiltered) by property type
-property_type_count <- eui_data_joined%>%
-  group_by(Property_type) %>%
-  summarise(Total = n()) # grouping the steam data into property type by count
-view(property_type_count)
-
-#Making tables for each property type- doing steam
-# filter each data by property types- then from each table find the outliers.
+#_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________
